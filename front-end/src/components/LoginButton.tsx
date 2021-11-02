614e595a228815990417ac4cbfd38127198ed99c
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2021-11-02 15:43:52 +0900

    <update>:<form tag -> fetch 비동기 서버 요청 방식 변경>
    <description>
    form tag로는 서버의 응답을 받는데 한계가 있음
    -> useState로 id, pwd 상태 관리 후 fetch 요청 보내는 형식으로 변경
    
    [#3, #4, #5]

--- front-end/src/components/LoginButton.tsx
+++ front-end/src/components/LoginButton.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import '../styles/LoginButton.css';
 
 const loginModal = () => {
@@ -6,17 +7,45 @@ const loginModal = () => {
 };
 
 const LoginButton = () => {
+  const [userInfo, setUserInfo] = useState({ id: '', pwd: '' });
+
+  const changeId = (e: any) => {
+    setUserInfo({ ...userInfo, id: e.target.value });
+  };
+
+  const changePwd = (e: any) => {
+    setUserInfo({ ...userInfo, pwd: e.target.value });
+  };
+
+  const requestToServer = async (e: any) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: userInfo['id'],
+        password: userInfo['pwd'],
+      }),
+    };
+    const data = await fetch('/users/id-check', options);
+    const user = await data.json(); // user = false 일 경우 오류 메시지 모달창
+    console.log(user);
+  };
+
   return (
     <>
       <button className="main-common-button main-login-button" onClick={loginModal}>
         {'로그인 하고 플레이!'}
       </button>
-      <form className="main-login-modal-hidden" action="/users/id-check" method="GET">
+      <div className="main-login-modal-hidden">
         <div className="main-login-header">Account Login</div>
-        <input className="main-login-id-password" type="text" name="id" placeholder="아이디를 입력하세요."></input>
-        <input className="main-login-id-password" type="password" name="password" placeholder="비밀번호를 입력하세요."></input>
-        <input className="main-login-submit" type="submit" value="Let's start lying...!"></input>
-      </form>
+        <input className="main-login-id-password" type="text" placeholder="아이디를 입력하세요." onInput={changeId}></input>
+        <input className="main-login-id-password" type="password" placeholder="비밀번호를 입력하세요." onInput={changePwd}></input>
+        <button className="main-login-submit" onClick={requestToServer}>
+          Let's start lying...!
+        </button>
+      </div>
     </>
   );
 };
