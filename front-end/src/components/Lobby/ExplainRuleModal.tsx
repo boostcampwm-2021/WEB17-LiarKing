72d92c0b7d63dcb157c5b0ee1b623f9c4281dfb2
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2021-11-08 19:09:33 +0900

    [feat] 룰 설명 이미지 추가
    룰 설명 모달창 각 페이지에 이미지 추가

--- front-end/src/components/Lobby/ExplainRuleModal.tsx
+++ front-end/src/components/Lobby/ExplainRuleModal.tsx
@@ -2,8 +2,18 @@ import '../../styles/ExplainRuleModal.css';
 import { useState } from 'react';
 import leftArrow from '../../images/leftArrow.svg';
 import rightArrow from '../../images/rightArrow.svg';
+import ruleOne from '../../images/ruleOne.svg';
+import ruletwo from '../../images/ruleTwo.svg';
+import ruleThree from '../../images/ruleThree.svg';
+import ruleFour from '../../images/ruleFour.svg';
+import ruleFive from '../../images/mainChar1.svg';
+import ruleSix from '../../images/mainChar2.svg';
 
 interface ruleInterface {
+  [prop: string]: string;
+}
+
+interface ruleImageInterface {
   [prop: string]: string;
 }
 
@@ -14,6 +24,15 @@ const rules: ruleInterface = {
   4: '4. 총 라운드 수 만큼 설명한 뒤, 제한 시간 이내에 라이어가 누구인지 투표합니다.',
   5: '5. 라이어가 아닌 사람이 뽑히거나 라이어가 마지막에 제시어를 맞히면 라이어의 승리입니다.',
   6: '6. 라이어가 제시어를 맞히지 못할 경우 일반 플레이어의 승리입니다.',
+};
+
+const ruleImages: ruleImageInterface = {
+  1: ruleOne,
+  2: ruletwo,
+  3: ruleThree,
+  4: ruleFour,
+  5: ruleFive,
+  6: ruleSix,
 };
 
 const ExplainRuleModal = () => {
@@ -33,8 +52,9 @@ const ExplainRuleModal = () => {
   };
 
   return (
-    <div id="explain-rule">
+    <div id="create-room">
       <div className="explain-rule-explanation">{rules[String(pageNumber)]}</div>
+      <img className="explain-rule-image" src={ruleImages[String(pageNumber)]} />
       <div className="room-list-buttons">
         <img className="room-list-arrows" src={leftArrow} onClick={decreasePage}></img>
         <img className="room-list-arrows" src={rightArrow} onClick={increasePage}></img>
