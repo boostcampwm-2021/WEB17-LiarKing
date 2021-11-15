1ec7fcb1c94421b1ee9e3d5ecbbb0b4f69163b71
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2021-11-15 17:38:25 +0900

    [feat] 게임방 설정 모달창 기능 구현 #112
    게임 설정 버튼 클릭 시 모달창 띄움
    - 카테고리, 플레이어 수, 라운드 수 변경 반영 가능, 리코일로 상태 관리

--- front-end/src/components/Game/GameButtons.tsx
+++ front-end/src/components/Game/GameButtons.tsx
@@ -1,22 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { useHistory } from 'react-router';
 import { useRecoilValue } from 'recoil';
 import { globalContext } from '../../App';
 import globalAtom from '../../recoilStore/globalAtom';
 
+import GameRoomSettings from './GameRoomSettings';
+
 const GameButtons = () => {
   const history = useHistory();
+  const [settingsModal, setSettingsModal] = useState([]);
   const { socket } = useContext(globalContext);
   const { selectedRoomTitle } = useRecoilValue(globalAtom.roomData);
+
+  const offModal = () => {
+    setSettingsModal([]);
+  };
 
   const exit = () => {
     socket.emit('room exit', selectedRoomTitle);
     history.replace('/lobby');
   };
 
+  const roomSettings = () => {
+    const ModalOutLocation = <section className="modal-outter" onClick={offModal} key={0} />;
+    setSettingsModal([ModalOutLocation, <GameRoomSettings offModal={offModal} key={1} />]);
+  };
+
   return (
     <div className="game-header-buttons">
-      <button className="game-header-button">게임 설정</button>
+      <button className="game-header-button" onClick={roomSettings}>
+        게임 설정
+      </button>
+      {settingsModal}
       <button className="game-header-button">준비 완료</button>
       <button className="game-header-button">게임 시작</button>
       <div className="game-header-button-exit" onClick={exit} />
