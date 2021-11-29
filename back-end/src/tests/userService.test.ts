f9530109d482a2ceaea2a9f47f7803a623dc4029
J065_김한빈 <rttr1023@gmail.com>
2021-11-29 18:33:41 +0900

    [test] 테스트 코드 수정
    테스트 메세지 수정

--- back-end/src/tests/userService.test.ts
+++ back-end/src/tests/userService.test.ts
@@ -3,27 +3,27 @@ import { User } from '../database/entity/User';
 import userService from '../database/service/userService';
 
 describe('getUsersRanks test', () => {
-  test('get top 5 users', async () => {
+  test('success (get top 5 users)', async () => {
     const ranks = await userService.getUsersRanks();
     expect(ranks).toHaveLength(5);
   });
 });
 
 describe('signUpUser test', () => {
-  test('sign up success', async () => {
+  test('success (sign up)', async () => {
     const user = await userService.signUpUser('signupid1', 'signuppw1');
     await getRepository(User).delete({ user_id: 'signupid1' });
     expect(user).toBeTruthy();
   });
 
-  test('sign up fail', async () => {
+  test('fail (sign up)', async () => {
     const user = await userService.signUpUser('testid1', 'testpw1');
     expect(user).toBeFalsy();
   });
 });
 
 describe('getUserInfo', () => {
-  test('get user Info', async () => {
+  test('success (get user Info)', async () => {
     const user = await userService.getUserInfo('asdfasdf');
     expect(user).toBeTruthy();
   });
--- back-end/src/tests/wordService.test.ts
+++ back-end/src/tests/wordService.test.ts
@@ -1,7 +1,7 @@
 import { getRandomWords } from '../database/service/wordService';
 
 describe('getRandomWords test', () => {
-  test('get word array of 15 length ', async () => {
+  test('success (get word array of 15 length)', async () => {
     const result = await getRandomWords('스포츠');
     expect(result).toHaveLength(15);
   });
