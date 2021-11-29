e1a95f706985939c289eda3e9118ddcebb688356
J065_김한빈 <rttr1023@gmail.com>
2021-11-29 18:27:48 +0900

    [test] 테스트 코드 추가
    서버쪽 테스트 코드 추가

--- back-end/src/tests/indexRouter.test.ts
+++ back-end/src/tests/indexRouter.test.ts
@@ -7,8 +7,7 @@ describe('POST /login test', () => {
       id: 'testid1',
       password: 'testpw1',
     });
-
-    expect(response.body).toBeTruthy();
+    expect(response.body.state).toBe('success');
   });
 
   test('incorrect id and password', async () => {
@@ -16,7 +15,56 @@ describe('POST /login test', () => {
       id: 'testid11111111',
       password: 'testpw1',
     });
+    expect(response.body.state).toBe('mismatch');
+  });
 
-    expect(response.body).toBeFalsy();
+  test('same id already logged in', async () => {
+    const response = await request(app).post('/api/login').send({
+      id: 'testid1',
+      password: 'testpw1',
+    });
+    expect(response.body.state).toBe('duplicated');
+  });
+});
+
+describe('POST /non-login test', () => {
+  test('success', async () => {
+    const response = await request(app).post('/api/non-login').send({
+      nickname: 'test-non-login',
+    });
+    expect(response.body.state).toBe('success');
+  });
+
+  test('same id exists in user database', async () => {
+    const response = await request(app).post('/api/non-login').send({
+      nickname: 'test-non-login',
+    });
+    expect(response.body.state).toBe('non-user logged in');
+  });
+
+  test('already same nickname logged in', async () => {
+    const response = await request(app).post('/api/non-login').send({
+      nickname: 'asdasdasd',
+    });
+    expect(response.body.state).toBe('user exist');
+  });
+});
+
+describe('POST /logout test', () => {
+  test('login and logout success', async () => {
+    await request(app).post('/api/login').send({
+      id: 'testid1',
+      password: 'testpw1',
+    });
+    const result = await request(app).post('/logout').send({ user_id: 'testid1' });
+    expect(result).toBeTruthy();
+  });
+
+  test('non-login and logout success', async () => {
+    await request(app).post('/api/non-login').send({
+      nickname: 'test-non-login',
+    });
+    const result = await request(app).post('/logout').send({ user_id: 'test-non-login' });
+    expect(result).toBeTruthy();
   });
 });
--- back-end/src/tests/userService.test.ts
+++ back-end/src/tests/userService.test.ts
@@ -1,8 +1,30 @@
+import { getRepository } from 'typeorm';
+import { User } from '../database/entity/User';
 import userService from '../database/service/userService';
 
-describe('GET /api/users/ranks', () => {
+describe('getUsersRanks test', () => {
   test('get top 5 users', async () => {
     const ranks = await userService.getUsersRanks();
     expect(ranks).toHaveLength(5);
+  });
+});
+
+describe('signUpUser test', () => {
+  test('sign up success', async () => {
+    const user = await userService.signUpUser('signupid1', 'signuppw1');
+    await getRepository(User).delete({ user_id: 'signupid1' });
+    expect(user).toBeTruthy();
+  });
+
+  test('sign up fail', async () => {
+    const user = await userService.signUpUser('testid1', 'testpw1');
+    expect(user).toBeFalsy();
+  });
+});
+
+describe('getUserInfo', () => {
+  test('get user Info', async () => {
+    const user = await userService.getUserInfo('asdfasdf');
+    expect(user).toBeTruthy();
   });
 });
