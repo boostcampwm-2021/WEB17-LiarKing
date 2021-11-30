50b83d8356bec78a2a04d94be0bdcf2bac8157e6
sumin123456 <soomin5234@gmail.com>
2021-12-01 01:48:35 +0900

    [feat] 모바일 환경 soft keyboard 대응
    해결 사항
    - 모바일 환경에서 soft keyboard가 생기면 css가 전부 찌그러지는 현상
    
    해결 방법
    - main div의 height를 화면의 orientation이 변할 때 마다 window.height로 px로 고정
    - 하위 요소들의 height가 vh였던 것을 전부 %로 수정
    
    추가로 해결해야 할 이슈
    - main화면에서 생기는 약간의 스크롤을 없앨 방법
    - vh -> % 로 수정하는 과정에서 css가 변경되어 재확인 필요

--- front-end/src/components/pages/Main.tsx
+++ front-end/src/components/pages/Main.tsx
@@ -2,9 +2,18 @@ import '../../styles/Main.css';
 import NoLoginButton from '../Main/NoLoginButton';
 import LoginButton from '../Main/LoginButton';
 import JoinButton from '../Main/JoinButton';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Main = () => {
+  useEffect(() => {
+    const main = document.querySelector<HTMLElement>('#main');
+    main.style.height = window.innerHeight.toString() + 'px';
+
+    window.addEventListener('orientationchange', function () {
+      main.style.height = window.innerWidth.toString() + 'px';
+    });
+  });
+
   return (
     <div id="main">
       <div className="main-header">Liar Game</div>
