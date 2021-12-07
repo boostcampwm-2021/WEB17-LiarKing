fa15b49c1ab278a004b0cf0596269eaea9486d44
J065_김한빈 <rttr1023@gmail.com>
2021-12-07 23:56:21 +0900

    [test] 테스트 코드 수정

--- back-end/src/tests/userService.test.ts
+++ back-end/src/tests/userService.test.ts
@@ -10,7 +10,7 @@ const newUserPassword = 'newtestpw1';
 describe('getUsersRanks', () => {
   test('success', async () => {
     const ranks = await userService.getUsersRanks();
-    expect(ranks).toBeGreaterThan(1);
+    expect(ranks.length).toBeGreaterThan(0);
   });
 });
 
